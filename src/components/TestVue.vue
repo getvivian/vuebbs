5e90261e24884a80f1821b7bfa6c296930510a37
unknown <1260127789@qq.com>
2018-05-29 22:16:01 +0800

    v2

---
+++ src/components/TestVue.vue
@@ -0,0 +1,25 @@
+<template>
+
+  <div id="note">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  name:"TestVue",
+  data(){
+    return {
+      msg:"hello test"
+    }
+  }
+}
+</script>
+
+
+<style  scoped>
+ #note{
+   font-size: 122px;
+   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+
+ }
+</style>
+
